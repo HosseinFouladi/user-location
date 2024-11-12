aa5b2fefc58f3dd9cd753bef1168c5ca4ccd3140
Hossein79 <hosseinfouladi20@gmail.com>
2024-11-12 12:21:51 +0330

    core:project setup completed

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-
+    <div class=" text-purple-500">
+hh
     </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  optimizeDeps: {
+    include: ['vue', 'leaflet', 'vue-leaflet']
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
